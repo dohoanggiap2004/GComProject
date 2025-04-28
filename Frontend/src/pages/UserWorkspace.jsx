199a4705713dbc2afd71601c738c45410839d9aa
dohoanggiap2004 <dohoanggiap2004@gmail.com>
2025-04-28 19:36:49 +0700

    fix: test in production

--- Frontend/src/pages/UserWorkspace.jsx
+++ Frontend/src/pages/UserWorkspace.jsx
@@ -4,12 +4,27 @@ import Dashboard from "../components/Workspace/Dashboard.jsx";
 import {useEffect} from "react";
 import {useDispatch} from "react-redux";
 import {getWorkspaceByMemberId} from "../store/actions/workspaceAction.jsx";
+import Cookies from "js-cookie";
 
 const UserWorkspace = () => {
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(getWorkspaceByMemberId())
     }, []);
+
+    useEffect(() => {
+        const accessToken = Cookies.get("accessToken");
+        console.log('cookie: ', accessToken)
+        // if (accessToken) {
+        //     dispatch(getUserInfo())
+        //     dispatch(loginUserSuccess());
+        // } else if (!accessToken && isLoginUser) {
+        //     getAccessToken();
+        // } else if (!isLoginUser) {
+        //     dispatch(logoutUser());
+        // }
+
+    }, [Cookies.get("accessToken")]);
     return (
         <>
             <NavbarWorkspace/>
