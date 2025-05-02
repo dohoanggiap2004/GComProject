6e9046fd6afcbf42512094838fe969beb1e78682
dohoanggiap2004 <dohoanggiap2004@gmail.com>
2025-05-02 22:00:54 +0700

    fix: test in product

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -38,7 +38,13 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Serve static files
-app.use("/", express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
+});
 
 //authenticate via passportjs
 localPassport()
