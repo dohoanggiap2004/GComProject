fbeca409e36c970fa742ee8019d7043cd744102e
dohoanggiap2004 <dohoanggiap2004@gmail.com>
2025-05-05 17:36:41 +0700

    fix: webhook

--- Backend/src/routers/api/payos.js
+++ Backend/src/routers/api/payos.js
@@ -2,8 +2,9 @@ const { payos } = require('../../config/payos')
 const express = require('express')
 const router = express.Router()
 const PayOsController = require('../../app/controllers/apiController/PayOsController')
+const verifyJWT = require('../../middlewares/verifyJWT')
 
-router.post('/create-payment-link', PayOsController.createPaymentLink)
+router.post('/create-payment-link', verifyJWT, PayOsController.createPaymentLink)
 router.post('/receive-hook', PayOsController.ReceiveWebhook)
 
 module.exports = router
