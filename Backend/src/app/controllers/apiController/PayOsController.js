91516b8d8489c3e86ae497f599d441745d3b63f8
dohoanggiap2004 <dohoanggiap2004@gmail.com>
2025-05-05 17:45:54 +0700

    fix: webhook

--- Backend/src/app/controllers/apiController/PayOsController.js
+++ Backend/src/app/controllers/apiController/PayOsController.js
@@ -51,34 +51,34 @@ class PayOsController {
                 return res.status(400).json({ message: "Missing data, signature, or userId" });
             }
             //validate signature
-            // const valid = isValidSignature(data, signature, process.env.PAYOS_CHECKSUM_KEY);
-            // if (!valid) {
-            //     return res.status(403).json({ message: "Invalid signature" });
-            // }
-            // await createWebhookService({
-            //     ...data,
-            //     success: req.body.success
-            // })
+            const valid = isValidSignature(data, signature, process.env.PAYOS_CHECKSUM_KEY);
+            if (!valid) {
+                return res.status(403).json({ message: "Invalid signature" });
+            }
+            await createWebhookService({
+                ...data,
+                success: req.body.success
+            })
 
-            // const transaction = await getTransactionByOrderCode(data.orderCode);
-            // if (!transaction) {
-            //     return res.status(404).json({ message: "Transaction not found" });
-            // }
+            const transaction = await getTransactionByOrderCode(data.orderCode);
+            if (!transaction) {
+                return res.status(404).json({ message: "Transaction not found" });
+            }
 
             // Kiểm tra trạng thái thành công và đúng số tiền
-            // if (req.body.success === true && transaction.amount === data.amount) {
-            //     const userUpdate = {
-            //         service: 'premium',
-            //         serviceExpiry: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
-            //     };
-            //
-            //     // Cập nhật cả status và user bằng transaction
-            //     await updateStatusAndUserService(data.orderCode, 'PAID', userUpdate);
-                console.log('webhook received:', data)
+            if (req.body.success === true && transaction.amount === data.amount) {
+                const userUpdate = {
+                    service: 'premium',
+                    serviceExpiry: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+                };
+
+                // Cập nhật cả status và user bằng transaction
+                await updateStatusAndUserService(data.orderCode, 'PAID', userUpdate);
+
                 return res.status(200).json({ message: "Transaction and user updated successfully" });
-            // } else {
-            //     return res.status(400).json({ message: "Transaction not valid or not successful" });
-            // }
+            } else {
+                return res.status(400).json({ message: "Transaction not valid or not successful" });
+            }
         } catch (error) {
             console.error("Error in ReceiveWebhook:", error);
             res.status(500).json({ message: "Internal server error", error: error.message });
